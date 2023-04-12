440d5322116ba1c6a7e1a6474f89133caa2f6e57
Eli Fuchsman <efuchsman@gmail.com>
2023-04-12 16:38:16 -0600

    create team_info

--- libraries/team_stats.py
+++ libraries/team_stats.py
@@ -8,3 +8,16 @@ class TeamStats(GameStats, LeagueStats):
         self.game_stats = GameStats()
         self.league_stats = LeagueStats()
         self.all_teams = self.league_stats.all_teams
+
+    def team_info(self, id):
+        team_hash = defaultdict(str)
+
+        for team in self.all_teams:
+            if team['team_id'] == id:
+                team_hash['team_id'] += team['team_id']
+                team_hash['franchise_id'] += team['franchiseId']
+                team_hash['team_name'] += team['teamName']
+                team_hash['abbreviation'] += team['abbreviation']
+                team_hash['stadium'] += team['Stadium']
+                team_hash['link'] += team['link']
+        return team_hash
--- tests/test_team_stats.py
+++ tests/test_team_stats.py
@@ -2,7 +2,6 @@ import unittest
 from libraries.team_stats import TeamStats
 from libraries.league_stats import LeagueStats
 from libraries.game_stats import GameStats
-from collections import defaultdict
 
 
 class TestTeamStats(unittest.TestCase):
@@ -13,3 +12,17 @@ class TestTeamStats(unittest.TestCase):
         self.assertIsInstance(team_stats.game_stats, GameStats)
         self.assertIsInstance(team_stats.league_stats, LeagueStats)
         self.assertEqual(len(team_stats.all_teams), 32)
+
+    def test_team_info(self):
+        team_stats = TeamStats()
+
+        expected = {
+            "team_id": "18",
+            "franchise_id": "34",
+            "team_name": "Minnesota United FC",
+            "abbreviation": "MIN",
+            "stadium": "Allianz Field",
+            "link": "/api/v1/teams/18"
+        }
+
+        self.assertEqual(team_stats.team_info("18"), expected)
